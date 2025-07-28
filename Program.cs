48fd73855f24fa994a516f192dc20572c9cbf7ca
yovani.vazquez <yovazbz@gmail.com>
2025-07-28 15:24:20 -0600

    Update - SICU

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Sicu.Modules.Swagger;
+using Tools;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,28 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwagger();
+
+// Obtiene la clave de cifrado del archivo de configuración.
+string? KGenerate = builder.Configuration.GetValue<string>("KGenerate");
+
+//// Obtiene las URL permitidas y las desencripta.
+List<string> originsList = new List<string>();
+var encryptedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+foreach (var item in encryptedOrigins)
+{
+    originsList.Add(new EncryptDecrypt().Decrypt(item, KGenerate));
+}
+string[]? decryptedOrigins = originsList.ToArray();
+
+// Configura las políticas de CORS.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("SitiosPermitidos",
+        builder => builder
+            .WithOrigins(decryptedOrigins ?? new string[] { "*" }) // Usamos "*" para permitir cualquier origen si AllowedOrigins es nulo
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -31,8 +54,15 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+// Configura el flujo de solicitud HTTP.
 app.UseHttpsRedirection();
-app.UseStaticFiles(); 
+app.UseStaticFiles();
+
+app.UseAuthentication();
 app.UseAuthorization();
+//app.UseCors("SitiosPermitidos");
+
+// Mapea los controladores.
 app.MapControllers();
+
 app.Run();
